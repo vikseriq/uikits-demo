dce586aaded054a55da4350e909857016d881bbf
Wiktor Serow <vikseriq@gmail.com>
2021-04-09 21:15:26 +0200

    Publish to gh-pages

---
+++ assets/main.5e56ea1c.js
@@ -0,0 +1,1 @@
+import"./dynamic-import-polyfill.feac7147.js";import{v as e}from"./vendor.ce0c5a8e.js";new e({el:"#app",data(){return{demos:[{code:"kit-bs",label:"Bootstrap"},{code:"kit-bue",label:"Buefy"},{code:"kit-vf",label:"Vuetify"},{code:"kit-ele",label:"Element"}]}},mounted(){this.$refs.iframe.contentDocument.body.innerHTML=this.$refs.iframe.innerHTML}});
